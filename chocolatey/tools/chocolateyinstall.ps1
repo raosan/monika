172c3d4b2ce0497080099bc19528f39b6565a56e
Raosan Fikri <raosan@hyperjump.tech>
2022-02-16 19:49:11 +0700

    use version 1.6.11

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://github.com/hyperjumptech/monika/releases/download/v1.6.12/monika-v1.6.12-win-x64.zip'
+$url64      = 'https://github.com/hyperjumptech/monika/releases/download/v1.6.11/monika-v1.6.11-win-x64.zip'
 
 $packageArgs = @{
   packageName   = 'monika'
   unzipLocation = $toolsDir
   url64bit      = $url64
-  checksum64    = 'https://github.com/hyperjumptech/monika/releases/download/v1.6.12/monika-v1.6.12-win-x64-CHECKSUM.txt'
+  checksum64    = 'DAA2C74C2665D82D5634ED7E6C1A6FEC2866D02A2F7FAF84A795EA9E0A317A66'
   checksumType64= 'sha256'
 }
 
